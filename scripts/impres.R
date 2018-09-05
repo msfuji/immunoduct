69e1c9cf8d4583f57705b265a374a8222c3edb0c
Masashi Fujita <mssfjt@gmail.com>
2018-09-05 16:34:39 +0900

    add IMPRES

--- scripts/impres.R
+++ scripts/impres.R
@@ -1,9 +1,9 @@
 source("scripts/common.R")
+library(tidyr)
 
-
-#args <- commandArgs(trailingOnly=T)
-infile <- "example/LUAD_n10.gct" #args[1]
-outfile <- "hoge.gct" #args[2]
+args <- commandArgs(trailingOnly=T)
+infile <- args[1]
+outfile <- args[2]
 
 df <- read_gct(infile)
 
@@ -32,3 +32,24 @@ if(length(missing) > 0){
   print(missing)
   stop("[ERROR] some IMPRES genes were not found in the input.")
 }
+
+df <- df %>% select(-Description) %>%
+  gather(key=sample, value=expression, -Name)
+
+imp.df <- imp %>%
+  inner_join(df, by=c("Gene1"="Name")) %>%
+  inner_join(df, by=c("Gene2"="Name", "sample"), suffix=c("1", "2"))
+
+#
+# compute IMPRES
+#
+res <- imp.df %>% group_by(sample) %>%
+  summarize(impres=sum(expression1 > expression2))
+
+#
+# save
+#
+v <- res$impres
+names(v) <- res$sample
+t <- data.frame(Name="IMPRES", Description="Signature", t(v), check.names=F)
+t %>% write_gct(outfile)
