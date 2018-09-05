d9139c9be4cd38d5ddb6612f890b67202ef2ce81
Masashi Fujita <mssfjt@gmail.com>
2018-09-05 12:51:43 +0900

    add cibersort

--- scripts/cibersort.R
+++ scripts/cibersort.R
@@ -19,8 +19,15 @@ df %>% select(-Description) %>% rename(GeneSymbol=Name) %>% write_tsv(tmpfile)
 res <- CIBERSORT("scripts/LM22.txt", tmpfile, perm=0, QN=F, absolute=F)
 
 #
+# move CIBERSORT-Results.txt to cell/ dir
+#
+file.rename("CIBERSORT-Results.txt", "cell/CIBERSORT-Results.txt")
+
+#
 # extract and save cell fractions
 #
-cf <- data.frame(Name=rownames(res), Description="xCell", res,
+res <- t(res)
+cf <- data.frame(Name=rownames(res), Description="CIBERSORT", res,
   row.names=NULL, check.names=F)
+cf <- cf %>% filter(!Name %in% c("P-value", "Correlation", "RMSE"))
 cf %>% write_gct(outfile)
